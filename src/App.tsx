4cc9457bf1f17558e29bce4ed2c264762631ca8a
raccoon-ccoder <qorwjddus96@naver.com>
2022-04-10 22:26:20 +0900

    220410_프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle, ThemeContext } from "styled-components";
+import reset from "styled-reset";
+import ToDoList from './ToDoList';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  a {
+      text-decoration: none;
+      color: inherit;
+    }
+  * {
+      box-sizing: border-box;
+    }
+  input {
+      border: none;
+  }
+  body {
+    background-color: ${props => props.theme.bgColor};
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme:DefaultTheme = {
+    bgColor: "#e6e9ef",
+    textColor: "#333", 
+    accentColor: "#5b73a3",
+    cardBgColor: "white",
+};
